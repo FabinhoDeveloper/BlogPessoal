3907ab52112ed0e188f609e09b25f1d0dde1f2ab
Fabio Ezequiel Teixeira dos Santos <fabioezequiel555@gmail.com>
2026-04-17 15:42:22 -0300

    Paleta de cores definida

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,30 @@
+import profile from '../assets/profile.jpg'
+
+import Navbar from '../components/Navbar'
+
 export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
             <header>
-                <nav className="flex flex-row justify-between items-center p-4 bg-amber-800 w-full">
-                    <div>
-                        <h1 className="text-white text-2xl font-bold">Fábio.dev</h1>
-                    </div>
-                    <div className="flex flex-row text-white gap-10">
-                        <a className="color-white" href="#sobre-mim">Sobre mim</a>
-                        <a href="#projetos">Projetos</a>
-                        <a href="#artigos">Artigos</a>
-                        <a href="#contato">Contato</a>
-                    </div>
-                </nav>
+                <Navbar />
             </header>
             <main>
-                <section id="sobre-mim" className="flex bg-gray-950 h-120 items-center justify-center">
+                <section id="sobre-mim" className="flex flex-row bg-gray-950 h-120 items-center justify-center">
                     <div>
-                        <img src="" alt="" />
+                        <img src={profile} alt="Foto de perfil" className="rounded-full w-32 h-32"/>
                     </div>
                     <div>
-
+                        <p className="text-white">Olá! Meu nome é Fábio Ezequiel, e sou desenvolvedor fullstack.</p>
                     </div>
-                    <h2 className="text-3xl font-bold text-white">Sobre mim!</h2>
                 </section>
                 <section id="projetos" className="flex bg-gray-900 h-120 items-center justify-center">
                     <h2 className="text-3xl font-bold text-white">Projetos!</h2>
+
                 </section>
-                <section id="artigos" className="flex bg-amber-400 h-120 items-center justify-center">
+                <section id="artigos" className="flex bg-gray-950 h-120 items-center justify-center">
                     <h2 className="text-3xl font-bold text-white">Artigos!</h2>
                 </section>
-                <section id="contato" className="flex bg-amber-500 h-120 items-center justify-center">
+                <section id="contato" className="flex bg-gray-900 h-120 items-center justify-center">
                     <div>
                         <form action="">
                             <label for="nome">Nome:</label>
@@ -38,12 +32,13 @@ export default function Home() {
                         </form>
                     </div>
                     <div>
+                        <img src="" alt="" />
                         <p>LinkedIn</p>
                     </div>
                 </section>
             </main>
             <footer>
-                <div className="bg-gray-900 h-30">
+                <div className="bg-gray-950 h-30">
                     <p className="text-center text-white py-4">© 2024 Fábio.dev. Todos os direitos reservados.</p>
                 </div>
             </footer>
